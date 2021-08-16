491a92d5a986ab2d101c6e0fc1c3e54d5ab51a70
Labiba <tabassum.30labiba@gmail.com>
2021-08-16 12:58:29 +0600

    fronted implemented

---
+++ admin/d_footer.php
@@ -0,0 +1,6 @@
+<footer class="sl-footer">
+    <div class="footer-left">
+      <div class="mg-b-2">Copyright &copy; 2021 CMS. All Rights Reserved.</div>
+      <div>Developed by CMS</div>
+    </div>
+</footer>
